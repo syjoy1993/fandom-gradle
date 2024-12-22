f3b2cbd38efeb6d200b675e8573c7a2fc33f0780
syjoy1993 <syjoy1993@gmail.com>
2024-12-22 19:10:04 +0900

    repository add method2

--- src/main/java/com/team5/fandom/repository/PostRepository.java
+++ src/main/java/com/team5/fandom/repository/PostRepository.java
@@ -1,6 +1,8 @@
 package com.team5.fandom.repository;
 
 import com.team5.fandom.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,7 @@ import java.util.List;
 public interface PostRepository extends JpaRepository<Post,Integer> {
     @Override
     List<Post> findAllById(Iterable<Integer> iterable);
+
+    @Override
+    Page<Post> findAll(Pageable pageable);
 }
